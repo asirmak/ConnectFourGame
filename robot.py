13e3f7d80e638ea6b97c59e2482b23fc5c20235a
Oğuzhan İçelliler <oguzhan.icelliler@gmail.com>
2023-12-01 10:20:37 +0100

    Use the new logging component

--- robot.py
+++ robot.py
@@ -1,10 +1,16 @@
 from pyniryo2 import *
 from roslibpy.core import RosTimeoutError
+from src.utils.Logging import CreateLogger
 import time
 import traceback
 import cv2
 class Robot:
     # Constant variables
+
+    # Logger for the robot
+    __logger = CreateLogger(
+        name="ROBOT"
+    )
 
     # Positions of certain objects
     __first_piece_pos = PoseObject(
@@ -21,23 +27,34 @@ class Robot:
         try:
             self.__robot = NiryoRobot(robot_ip)
         except:
-            raise ConnectionError("Connection to robot failed!")
+            self.__logger.critical("Robot connection failed, check if the ip addr is correct")
+            raise
         
         try:
             # Calibrate the robot
             self.__executeRobotAction(
                 self.__robot.arm.calibrate_auto
             )
+            self.__logger.info("Arm is calibrated and ready to use")
 
             # Detect the currently attached tool
             self.__executeRobotAction(
                 self.__robot.tool.update_tool
             )
+            current_tool = self.__executeRobotAction(
+                self.__robot.tool.get_current_tool_id
+            )
+            # Open the gripper
+            self.__executeRobotAction(
+                self.__robot.tool.release_with_tool
+            )
+            self.__logger.info("Gripper is ready to use")
 
             # Set up the conveyer belt
             self.__conveyor_id = self.__executeRobotAction(
                 self.__robot.conveyor.set_conveyor
             )
+            self.__logger.info("Conveyer belt ready to use")
 
             # Move robot to its default position
             self.__moveToHome()
@@ -58,12 +75,12 @@ class Robot:
                 result = action(*args)
                 action_retry = False
             except TypeError:
-                print("You did a coding error, do not pass function call instead pass a function reference")
+                self.__logger.critical("You did a coding error, do not pass function call instead pass a function reference")
                 raise
             except RosTimeoutError as e:
                 # robot internal bug safe to ignore
                 # TODO in the future maybe add a retry limit
-                print(e)
+                self.__logger.warning("Robot internal timing bug, safe to ignore, retrying action...")
                 continue
         return result
 
@@ -72,29 +89,32 @@ class Robot:
         self.__executeRobotAction(
             self.__robot.arm.move_to_home_pose
         )
+        self.__logger.info("Moved to home position")
 
     # Move robot to specified position
-    def __moveToPos(self, pos):
+    def __moveToPos(self, pos: PoseObject):
         self.__executeRobotAction(
             self.__robot.arm.move_pose, pos
         )
+        self.__logger.info(f"Moved to position {pos}")
     
     # Belt set up function to place the piece on the belt before the game starts
     def setUpBelt(self, piece_count = 21, force = False):
         if not self.__beltSetUp or force:
             currentMoveCount = 0
             while piece_count:
-                print(f"Currently setting up piece {self.__currentPiece}")
+                self.__logger.info(f"Currently setting up piece {self.__currentPiece}")
                 piece_count -= 1
                 
                 # Determine which place to show to user
                 index = self.__currentPiece % 2
-                print(f"The piece will be placed on position {index}")
+                self.__logger.info(f"The piece will be placed on position {index}")
                 self.__currentPiece += 1
 
                 # Move the robot arm to that position and wait for user input
                 self.__moveToPos(self.__first_piece_pos if index==0 else self.__second_piece_pos)
-                input("Press enter to continue for the next piece...")
+                self.__logger.info("Press enter to continue for the next piece...")
+                input()
                 currentMoveCount += 1
 
                 # Move to home after piece is placed
@@ -102,9 +122,9 @@ class Robot:
 
                 # Move the belt since 2 pieces were placed
                 if currentMoveCount == 2:
-                    print("Current stack is full moving pieces to the left")
+                    self.__logger.info("Piece stack full moving pieces to the left")
                     currentMoveCount = 0
-                    self.__movePieces(ConveyorDirection.BACKWARD)
+                    self.__movePiecesOnBelt(ConveyorDirection.BACKWARD)
             
             self.__beltSetUp = True
 
@@ -119,7 +139,7 @@ class Robot:
         # TODO raise the piece
 
     # Move pieces in the belt
-    def __movePieces(self, direction: ConveyorDirection):
+    def __movePiecesOnBelt(self, direction: ConveyorDirection):
         self.__executeRobotAction(
             self.__robot.conveyor.run_conveyor, self.__conveyor_id, 25, direction
         )
@@ -132,23 +152,21 @@ class Robot:
     def endRobot(self):
         self.__robot.end()
 
+
+# Test function for robot
 def __robotTest(ip):
+    test_logger = CreateLogger("ROBOT_TEST")
     try:
         robot_ethernet = None
         try:
             robot_ethernet = Robot(ip)
-        except ConnectionError as e:
-            
-            sys.exit(1)
-        except Exception as e:
-            print("Robot calibration functions failed!")
-            print("Please make sure that the area around the robot is clear")
-            traceback.print_exc(e)
+        except Exception:
+            test_logger.exception("Robot init failed!")
             sys.exit(1)
         
         robot_ethernet.setUpBelt()
     except KeyboardInterrupt:
-        print("\nProgram ended with keyboard interrupt")
+        test_logger.info("Program ended with keyboard interrupt")
         sys.exit(130)
     finally:
         if robot_ethernet is not None:
