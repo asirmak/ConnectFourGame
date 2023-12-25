76f32bbb9694eec77d5e41f0ebe4026b03dc5b0e
Oğuzhan İçelliler <oguzhan.icelliler@gmail.com>
2023-12-25 17:27:51 +0100

    Create setup.py for the project

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    # Project name
+    name="connect4game",
+
+    # Project version
+    version="0.1.0",
+
+    # Packages setup
+    packages=find_packages()
+)
